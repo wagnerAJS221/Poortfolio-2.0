640a9584cff7781a5702f972829931fc33d2224c
wagnerAJS221 <wagneralex221@gmai.com>
2023-07-11 07:56:31 +0100

    cube animation for tech stack done

--- react-portfolio/src/components/About/index.js
+++ react-portfolio/src/components/About/index.js
@@ -1,7 +1,16 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useState, useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faCss3,
+  faGitSquare,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
 
 function About() {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -25,6 +34,28 @@ function About() {
         <p>About me text to go here</p>
         <p>2nd About me text to go here </p>
         <p>3rd About me text to go here</p>
+      </div>
+      <div className="stage-cube-cont">
+        <div className="cubespinner">
+          <div className="face1">
+            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+          </div>
+          <div className="face2">
+            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+          </div>
+          <div className="face3">
+            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+          </div>
+          <div className="face4">
+            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+          </div>
+          <div className="face5">
+            <FontAwesomeIcon icon={faGitSquare} color="#EC4D28" />
+          </div>
+          <div className="face6">
+            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+          </div>
+        </div>
       </div>
     </div>
   )
