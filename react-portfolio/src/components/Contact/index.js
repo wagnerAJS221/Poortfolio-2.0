610132de63ba451ce3284b3d6b589da121499104
wagnerAJS221 <wagneralex221@gmai.com>
2023-07-12 15:16:43 +0100

    contact page underway and busy styling it now.

---
+++ react-portfolio/src/components/Contact/index.js
@@ -0,0 +1,69 @@
+import './index.scss'
+import Loader from 'react-loaders'
+import { useState, useEffect } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  //   useEffect(() => {
+  //     return setTimeout(() => {
+  //       setLetterClass('text-animate-hover')
+  //     }, 3000)
+  //   }, [])
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              StrArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>Reasons for contacting me, go here</p>
+          <div className="contact-form">
+            <form>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="message"
+                    name="name"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Contact
